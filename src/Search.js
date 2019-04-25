1c7937869209c676189c7103d26f1c70aabe6171
Bogdan Savluk <savluk.bogdan@gmail.com>
2019-04-25 11:21:25 +0200

    cleanup search component add more tests

--- src/Search.js
+++ src/Search.js
@@ -6,64 +6,84 @@ import SearchLoader from './SearchLoader';
 import Preview, { estimatePreviewSize } from './Preview';
 import { Col, Row } from 'antd';
 
+export function viewProps(props, view) {
+  const { getItem, query } = props;
+
+  let totalCount, isItemLoaded, loadMoreItems;
+  let PreviewRow, getItemSize;
+  if (view === 'list') {
+    totalCount = props.totalCount;
+    isItemLoaded = props.isItemLoaded;
+    loadMoreItems = props.loadMoreItems;
+
+    PreviewRow = ({ index, style }) => (
+      <div style={style}>
+        <Preview data={getItem(index)} />
+      </div>
+    );
+
+    getItemSize = index => estimatePreviewSize(getItem(index));
+  } else {
+    const count = 3;
+    totalCount = Math.ceil(props.totalCount / count);
+    const getIndices = index => {
+      const indices = [];
+      for (let i = 0; i < count && index * count + i < totalCount; i += 1)
+        indices.push(index * count + i);
+      return indices;
+    };
+
+    isItemLoaded = index => getIndices(index).every(props.isItemLoaded);
+
+    loadMoreItems = (start, stop) =>
+      props.loadMoreItems(start * count, stop * count);
+
+    PreviewRow = ({ index, style }) => (
+      <div style={style}>
+        <Row>
+          {getIndices(index).map(i => (
+            <Col span={24 / count} key={i}>
+              <Preview data={getItem(i)} />
+            </Col>
+          ))}
+        </Row>
+      </div>
+    );
+
+    getItemSize = index =>
+      Math.max(
+        0,
+        ...getIndices(index).map(i => estimatePreviewSize(getItem(i)))
+      );
+  }
+  return {
+    key: query + '__' + view,
+    totalCount,
+    isItemLoaded,
+    loadMoreItems,
+    PreviewRow,
+    getItemSize,
+  };
+}
+
 function Search({ query, view }) {
   return (
     <SearchLoader query={query}>
       {props => {
-        let { getItem, query } = props;
-        let totalCount, isItemLoaded, loadMoreItems;
-        let PreviewRow, getItemSize;
-        if (view === 'list') {
-          totalCount = props.totalCount;
-          isItemLoaded = props.isItemLoaded;
-          loadMoreItems = props.loadMoreItems;
-
-          PreviewRow = ({ index, style }) => (
-            <div style={style}>
-              <Preview data={getItem(index)} />
-            </div>
-          );
-
-          getItemSize = index => estimatePreviewSize(getItem(index));
-        } else {
-          const count = 3;
-          totalCount = Math.ceil(props.totalCount / count);
-          const getIndices = index => {
-            const indices = [];
-            for (let i = 0; i < count && index * count + i < totalCount; i += 1)
-              indices.push(index * count + i);
-            return indices;
-          };
-
-          isItemLoaded = index => getIndices(index).every(props.isItemLoaded);
-
-          loadMoreItems = (start, stop) =>
-            props.loadMoreItems(start * count, stop * count);
-
-          PreviewRow = ({ index, style }) => (
-            <div style={style}>
-              <Row>
-                {getIndices(index).map(i => (
-                  <Col span={24 / count} key={i}>
-                    <Preview data={getItem(i)} />
-                  </Col>
-                ))}
-              </Row>
-            </div>
-          );
-
-          getItemSize = index =>
-            Math.max(
-              0,
-              ...getIndices(index).map(i => estimatePreviewSize(getItem(i)))
-            );
-        }
+        const {
+          key,
+          totalCount,
+          isItemLoaded,
+          loadMoreItems,
+          PreviewRow,
+          getItemSize,
+        } = viewProps(props, view);
         return (
           <AutoSizer>
             {({ width, height }) => (
               // key to force recreating loader component after query or view changed
               <InfiniteLoader
-                key={query}
+                key={key}
                 isItemLoaded={isItemLoaded}
                 itemCount={totalCount}
                 loadMoreItems={loadMoreItems}
--- src/Search.test.js
+++ src/Search.test.js
@@ -1,9 +1,86 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Search from './Search';
+import Search, { viewProps } from './Search';
+import * as previewModule from './Preview';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Search view={'list'} query={''} />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('Search', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('correctly transform props for list view', () => {
+    const mockEstimatePreviewSize = jest
+      .spyOn(previewModule, 'estimatePreviewSize')
+      .mockImplementation(() => {
+        return 100;
+      });
+
+    const props = {
+      getItem: id => ({
+        id,
+      }),
+      query: 'test',
+      totalCount: 100,
+      isItemLoaded: () => false,
+      loadMoreItems: () => {},
+    };
+    const {
+      key,
+      totalCount,
+      isItemLoaded,
+      loadMoreItems,
+      getItemSize,
+    } = viewProps(props, 'list');
+
+    expect(key).toBe('test__list');
+    expect(totalCount).toEqual(props.totalCount);
+    expect(isItemLoaded).toEqual(props.isItemLoaded);
+    expect(loadMoreItems).toEqual(props.loadMoreItems);
+    expect(getItemSize(1)).toEqual(100);
+    expect(mockEstimatePreviewSize).toBeCalledWith({ id: 1 });
+  });
+
+  it('correctly transform props for grid view', () => {
+    const mockEstimatePreviewSize = jest
+      .spyOn(previewModule, 'estimatePreviewSize')
+      .mockImplementation(({ id }) => {
+        if (id === 0) return 100;
+        if (id === 1) return 200;
+        if (id === 2) return 300;
+      });
+
+    const props = {
+      getItem: id => ({
+        id,
+      }),
+      query: 'test',
+      totalCount: 100,
+      isItemLoaded: jest.fn(() => true),
+      loadMoreItems: jest.fn(() => false),
+    };
+    const {
+      key,
+      totalCount,
+      isItemLoaded,
+      loadMoreItems,
+      getItemSize,
+    } = viewProps(props, 'grid');
+
+    expect(key).toBe('test__grid');
+    expect(totalCount).toEqual(Math.ceil(props.totalCount / 3));
+    isItemLoaded(0);
+    expect(props.isItemLoaded).toHaveBeenCalledTimes(3);
+
+    loadMoreItems(0, 1);
+    expect(props.loadMoreItems).toBeCalledWith(0, 3);
+
+    expect(getItemSize(0)).toEqual(300);
+    expect(mockEstimatePreviewSize).toBeCalledWith({ id: 1 });
+  });
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Search view={'list'} query={''} />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
 });
